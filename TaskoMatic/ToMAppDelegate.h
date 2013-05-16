c739901fc13e4174434e9629634c22017d8f84b3
mwgarwood <mike@garwoods.net>
2013-05-16 14:57:24 -0500

    Initial Commit

---
+++ TaskoMatic/ToMAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  ToMAppDelegate.h
+//  TaskoMatic
+//
+//  Created by Mike Garwood on 5/16/13.
+//  Copyright (c) 2013 Neterata. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ToMAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ TaskoMatic/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  TaskoMatic
+//
+//  Created by Mike Garwood on 5/16/13.
+//  Copyright (c) 2013 Neterata. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "ToMAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([ToMAppDelegate class]));
+    }
+}
